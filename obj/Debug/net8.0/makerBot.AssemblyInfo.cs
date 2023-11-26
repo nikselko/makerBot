46c68ce27ffab45f325a73b84572032aad6c2204
Vani <Vani@DESKTOP-KDLKR94>
2023-11-26 18:18:58 +0100

    testr

--- obj/Debug/net8.0/makerBot.AssemblyInfo.cs
+++ obj/Debug/net8.0/makerBot.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("makerBot")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4c3b5f02f382557373200de2d4c9a4bd7192649")]
 [assembly: System.Reflection.AssemblyProductAttribute("makerBot")]
 [assembly: System.Reflection.AssemblyTitleAttribute("makerBot")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
